7ecc3f159776a8fdd6e4f06106a69e8ff9ff7435
Hemanth Madu <173272876+hemu-madu@users.noreply.github.com>
2025-12-23 12:48:41 +0530

    Configure for GitHub Pages deployment

--- server/index.ts
+++ server/index.ts
@@ -56,14 +56,12 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled in Replit
+  // serve the app on port 5000
   const port = 5000;
   server.listen({
     port,
     host: "0.0.0.0",
   }, () => {
-    log(`Server is now running at http://localhost:${port} and on Replit's public URL`);
+    log(`Server is now running at http://localhost:${port}`);
   });
 })();
--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   },
 
   build: {
-    outDir: path.resolve(__dirname, "docs"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
